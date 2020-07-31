c55960b4d09a91112f3c94b40c9bcf158e8862e9
butfrenplease <68925044+butfrenplease@users.noreply.github.com>
2020-07-31 08:04:35 +0300

    Update solution.py

--- Ex.2/solution.py
+++ Ex.2/solution.py
@@ -1,17 +1,16 @@
 import sys
 
-n = sys.argv[1]
-n = int(n)
+n = int(sys.argv[1])
+
 for i in range(n):
     while i < n:
       i += 1
       if (n > 1) and (i != n):
         print(' ' * (n-i-1), '#' * i)
-      elif (n>1) and (i == n):
+      elif (n > 1) and (i == n):
           print('#' * n)
       else:
           if n == 1:
               print('#')
-          break
     break
 
