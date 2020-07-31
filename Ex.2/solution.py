4bc11b8ef93c5aa14ab6b613cb503dbbae0da99a
butfrenplease <68925044+butfrenplease@users.noreply.github.com>
2020-07-31 06:28:06 +0300

    Add files via upload
    Необходимо написать скрипт, который «нарисует» (выведет на консоль) лестницу. Количество ступенек в лестнице передается скрипту в качестве параметра. Гарантируется, что на вход подаются только целые числа > 0.﻿ Чтение данных нужно произвести способом, аналогичным тому, что описан в предыдущем задании. Ступени должны отображаться с помощью символа решетки  "#" и пробелов.

---
+++ Ex.2/solution.py
@@ -0,0 +1,17 @@
+import sys
+
+n = sys.argv[1]
+n = int(n)
+for i in range(n):
+    while i < n:
+      i = i + 1
+      if (n > 1) and (i != n):
+        print(' ' * (n-i-1), '#' * i)
+      elif (n>1) and (i == n):
+          print('#' * n)
+      else:
+          if n == 1:
+              print('#')
+          break
+    break
+
