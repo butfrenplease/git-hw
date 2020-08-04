cbe9bd88fb79161bd14769cb696bfd11992dc04f
butfrenplease <68925044+butfrenplease@users.noreply.github.com>
2020-08-05 01:57:05 +0300

    Add files via upload

---
+++ Ex.6/to_json.py
@@ -0,0 +1,8 @@
+from functools import wraps
+import json
+
+def to_json(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return json.dumps(func(*args, **kwargs))
+    return wrapper
